f51dff13c5d121c57f9dda0b4508ad74f2e84693
chensam93 <56238240+chensam93@users.noreply.github.com>
2021-05-22 18:41:05 -0700

    Add files via upload

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+=======
+from django.apps import AppConfig
+
+
+class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main'
+>>>>>>> d9ed6763c3ef22f2118468c9f040b215af1b5c77
