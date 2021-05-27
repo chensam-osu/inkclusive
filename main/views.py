ce822f1ba5937de6d6aecf8223ae29f9fe16a3b8
Quazi Safin <41090756+Qsafin@users.noreply.github.com>
2021-05-27 14:21:58 -0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,8 +9,44 @@ import en_core_web_sm
 
 from .models import Words
 
+def clear_punctuation(word):
+
+    punctuation= '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    for letter in word: 
+
+        if letter in punctuation: 
+            word = word.replace(letter, "") 
+    
+    return word
+
+def format_word(word):
+
+    punctuation_list = ('!','(',')','-','[',']','{','}',';',':',"'",'"','\\',',','<','>','.','/','?','@','#','$','%','^','&','*','_','~')
+
+    new_word = ""
+
+    if word.startswith(punctuation_list) or word.endswith(punctuation_list):
+
+        for punctuation in punctuation_list:
+
+            if word.startswith(punctuation):
+                word = word.replace(punctuation, f"{punctuation}*")
+                new_word += word + "*"
+
+            elif word.endswith(punctuation):
+                new_word += "*"
+                word = word.replace(punctuation, f"*{punctuation}")
+
+                new_word += word
+
+    else:
+        new_word += "*" + word + "*"
+
+    return new_word
 
 
+Words.objects.distinct
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -56,43 +92,73 @@ class IndexView(TemplateView):
             else:
                 words = line.split()
                 temp_list = []
+                original_list = []
 
+                # LOOP THROUGH EACH WORD.
                 for word in words:
-                    in_db = Words.objects.all().filter(word__icontains=word)
 
-                    if pf._is_profane_word('en', word) or not in_db:
+                    clean_word = clear_punctuation(word)
+
+                    in_db = Words.objects.all().filter(word__icontains=clean_word)
+
+                    # WORD IS IN DATABASE
+                    if in_db:
+
+                        temp_list.append(clean_word)
+
                         temp_word = " ".join(temp_list)
-                        completed = Words.objects.all().filter(word__iexact=temp_word)
 
-                        if completed:
-                            new_word = "*" + temp_word + "*"
-                            wordlist.append(new_word)
-                            temp_list.clear()
-                        else:
-                            wordlist.append(temp_word)
-                            temp_list.clear()
 
-                        if pf._is_profane_word('en', word):
-                            new_word = "*" + word + "*"
-                            wordlist.append(new_word)
+                        starting_phrase = Words.objects.all().filter(word__startswith=temp_word)
+
+                        # CURRENT WORD IS THE START OF THE PHRASE
+                        if starting_phrase:
+
+                            original_list.append(word)
+
+                            completed = Words.objects.all().filter(word__iexact=temp_word)
+
+                            # CURRENT PHRASE IS COMPLETED
+                            if completed:
+                                original = " ".join(original_list)
+                                original_list.clear()
+
+                                new_word = format_word(original)
+                                wordlist.append(new_word)
+
+                                temp_list.clear()
+
+                            # TEMP WORD DID NOT COMPLETE THE PHRASE
+                            # else:
+                            #     temp_list.clear()
+                            #     temp_list.append(temp_word)
+                                
+                        
+                        # NOT START OF PHRASE KEEP GOING
                         else:
                             wordlist.append(word)
-                    else:
-                        temp_list.append(word)
-                
-                if len(temp_list) > 0:
-                    temp_word = " ".join(temp_list)
-                    completed = Words.objects.all().filter(word__iexact=temp_word)
-
-                    if completed:
-                            new_word = "*" + temp_word + "*"
-                            wordlist.append(new_word)
                             temp_list.clear()
-                    else:
+                            original_list.clear()
+
+                    # WORD IS A PROFANITY
+                    elif pf._is_profane_word('en', clean_word):
+
+                        temp_word = " ".join(temp_list)
                         wordlist.append(temp_word)
+
+                        new_word = format_word(word)
+                        wordlist.append(new_word)
                         temp_list.clear()
+                        continue
 
+                    # JUST A REGULAR WORD
+                    else:
+                        temp_word = " ".join(temp_list)
+                        wordlist.append(temp_word)
 
+                        wordlist.append(word)
+
+                        temp_list.clear()
 
         
         context["results"] = " ".join(wordlist)
